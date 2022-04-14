59a3001547ba732a084e5f9412399e0e4268bf78
iimm <liudichen@foxmail.com>
2022-04-14 16:31:57 +0800

    regeneratorRuntime is not defined

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,9 +3,15 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-14 11:01:55
- * @LastEditTime: 2022-04-14 13:55:27
+ * @LastEditTime: 2022-04-14 16:30:04
  */
 export default {
   esm: 'babel',
   cjs: 'babel',
+  extraBabelPresets:[
+    // '@babel/preset-env',
+  ],
+  extraBabelPlugins: [
+    "@babel/plugin-transform-runtime",
+  ]
 };
