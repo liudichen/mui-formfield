7dbcc41b7e766b1c5995fc63d70e62e647144a85
iimm <liudichen@foxmail.com>
2022-04-15 17:29:09 +0800

    space 更新d.ts

--- src/Space/index.d.ts
+++ src/Space/index.d.ts
@@ -3,23 +3,26 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-14 13:41:40
- * @LastEditTime: 2022-04-15 12:50:58
+ * @LastEditTime: 2022-04-15 17:28:17
  */
-import * as React from 'react';
+import React from 'react';
 
 import { sxType } from '../types';
 
-declare type stringSize = 'small' | 'medium' | 'large';
-declare type sizeType = stringSize | number;
-declare type sizeProp = sizeType | [sizeType, sizeType];
-interface spaceProps {
+type stringSize = 'small' | 'medium' | 'large';
+type sizeType = stringSize | number;
+type sizeProp = sizeType | [sizeType, sizeType];
+
+interface SpaceProps {
   size?: sizeProp;
   direction?: 'row' | 'column';
-  children: React.ReactNode;
+  // children: React.ReactNode;
   flexDirection?: any;
   split?: React.ReactNode;
   display?: string,
   sx?: sxType,
 }
 
-export default function Space(props:SpaceProps):JSX.Element;
+declare const Space: React.FunctionComponent<SpaceProps>;
+
+export default Space;
