b97bd7318e33ce3b6afa897554a284c5fb86021e
iimm <liudichen@foxmail.com>
2022-04-15 14:59:54 +0800

    omit已有属性

--- src/CheckboxGroup/index.d.ts
+++ src/CheckboxGroup/index.d.ts
@@ -3,7 +3,7 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-15 14:33:43
- * @LastEditTime: 2022-04-15 14:47:42
+ * @LastEditTime: 2022-04-15 14:58:46
  */
 import { CheckboxProps } from '@mui/material';
 import { FieldWrapperRelateProps, fieldCommonProps, optionsRelateProps, colorType } from '../types';
@@ -17,7 +17,7 @@ interface CheckboxGroupProps extends FieldWrapperRelateProps, optionsRelateProps
   maxCount?: number,
   color?: colorType,
   size?: sizeString | string,
-  itemProps?: CheckboxProps,
+  itemProps?: Omit<Omit<Omit<Omit<Omit<CheckboxProps, 'size'>, 'checked'>, 'color'>, 'onChange'>, 'disabled'>,
 }
 
 function CheckboxGroup(props: CheckboxGroupProps): JSX.Element;
