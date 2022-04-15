ea2b381317af88735383d252538029ba2dc098c4
iimm <liudichen@foxmail.com>
2022-04-15 14:15:04 +0800

    fieldlayout d.ts

---
+++ src/FieldLayout/index.d.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description:
+ * @Author: 柳涤尘 https://www.iimm.ink
+ * @LastEditors: 柳涤尘 liudichen@foxmail.com
+ * @Date: 2022-04-15 14:04:53
+ * @LastEditTime: 2022-04-15 14:12:54
+ */
+import { GridProps } from '@mui/material';
+
+interface FieldLayoutProps extends GridProps {
+  fullWidth?: boolean,
+}
+
+export default function FieldLayout(props: FieldLayoutProps): JSX.Element;
--- src/FieldLayout/utils/index.d.ts
+++ src/FieldLayout/utils/index.d.ts
@@ -3,5 +3,16 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-15 14:01:27
- * @LastEditTime: 2022-04-15 14:01:28
+ * @LastEditTime: 2022-04-15 14:07:21
  */
+type colSize = 'auto' | number | boolean;
+
+interface itemProps {
+  xs?: colSize,
+  sm?: colSize,
+  md?: colSize,
+  lg?: colSize,
+  xl?: number
+}
+
+export default function getItemColProps(props: itemProps): itemProps;
