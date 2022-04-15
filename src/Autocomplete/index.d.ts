b81e3c402a7f89e7ad2018f25c716ad6f7a3d9d4
iimm <liudichen@foxmail.com>
2022-04-15 14:54:33 +0800

    onChange ts

--- src/Autocomplete/index.d.ts
+++ src/Autocomplete/index.d.ts
@@ -3,15 +3,16 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-15 14:26:56
- * @LastEditTime: 2022-04-15 14:31:42
+ * @LastEditTime: 2022-04-15 14:53:58
  */
 import { AutocompleteProps as MuiProps } from '@mui/material';
 import { FieldWrapperRelateProps } from '../types';
 
-interface AutocompleteProps extends MuiProps, FieldWrapperRelateProps {
+interface AutocompleteProps extends Omit<MuiProps, 'onChange'>, FieldWrapperRelateProps {
   readonly?: boolean,
   request?: () => any[],
   placeholder?: string,
+  onChange?: (value:any) => void,
 }
 
 export default function Autocomplete(props: AutocompleteProps): JSX.Element;
