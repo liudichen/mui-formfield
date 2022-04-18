bfe26d560a328f9b70b2909c3ebb760ccd3357a9
iimm <liudichen@foxmail.com>
2022-04-18 16:57:04 +0800

    新增Transfer导出，版本升为0.1.11

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-14 11:01:55
- * @LastEditTime: 2022-04-15 18:55:11
+ * @LastEditTime: 2022-04-18 16:56:03
  */
 export { default as Autocomplete, AutocompleteProps } from './Autocomplete';
 export { default as CheckboxGroup, CheckboxGroupProps } from './CheckboxGroup';
@@ -18,3 +18,4 @@ export { default as Switch, SwitchProps } from './Switch';
 export { default as TextField, TextFieldProps } from './TextField';
 export { default as ToggleButtonGroup, ToggleButtonGroupProps } from './ToggleButtonGroup';
 export { default as UploadButton, UploadButtonProps } from './UploadButton';
+export { default as Transfer, TransferProps } from './Transfer';
