9259a097d6293956ccf0fa3234b544c52a466691
iimm <liudichen@foxmail.com>
2022-05-10 12:27:08 +0800

    ->0.2.15 移除Datepicker的dayjs格式转化, DateLocalizationProvider提供X和LAB两层context

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-14 11:01:55
- * @LastEditTime: 2022-05-09 21:11:35
+ * @LastEditTime: 2022-05-10 12:24:39
  */
 export { default as Autocomplete, AutocompleteProps } from './Autocomplete';
 export { default as CheckboxGroup, CheckboxGroupProps } from './CheckboxGroup';
-export { default as DateLocalizationProvider } from './DateLocalizationProvider';
+export { default as DateLocalizationProvider, DateLocalizationProviderProps } from './DateLocalizationProvider';
 export { default as DatePicker, DatePickerProps } from './DatePicker';
 export { default as DateRangePicker, DateRangePickerProps } from './DateRangePicker';
 export { default as FieldAction, FieldActionProps } from './FieldAction';
