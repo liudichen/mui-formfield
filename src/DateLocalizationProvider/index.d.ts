13a1be25fb2534a412f3a04336fddb65893136f0
iimm <liudichen@foxmail.com>
2022-05-10 12:24:11 +0800

    DateLocalizationProvider提供X 和Lab两层

--- src/DateLocalizationProvider/index.d.ts
+++ src/DateLocalizationProvider/index.d.ts
@@ -3,8 +3,18 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-15 14:49:21
- * @LastEditTime: 2022-05-05 17:24:24
+ * @LastEditTime: 2022-05-10 12:22:52
  */
-import { LocalizationProviderProps } from '@mui/x-date-pickers';
+import { LocalizationProviderProps as LocalizationProviderPropsX } from '@mui/x-date-pickers';
+import { AdapterDayjs as AdapterDayjsX } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProviderProps as LocalizationProviderPropsLab } from '@mui/lab';
+import AdapterDayjsLab from '@mui/lab/AdapterDayjs';
 
-export default function DateLocalizationProvider(props: LocalizationProviderProps): JSX.Element;
+export interface DateLocalizationProviderProps {
+  xProps?: Omit<LocalizationProviderPropsX, 'dateAdapter'>,
+  labProps?: Omit<LocalizationProviderPropsLab, 'dateAdapter'>,
+  dateAdapterX?: typeof AdapterDayjsX,
+  dateAdapterLab?: typeof AdapterDayjsLab,
+}
+
+export default function DateLocalizationProvider(props: DateLocalizationProviderProps): JSX.Element;
--- src/DateLocalizationProvider/index.jsx
+++ src/DateLocalizationProvider/index.jsx
@@ -3,23 +3,28 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-11 22:47:37
- * @LastEditTime: 2022-05-05 17:25:45
+ * @LastEditTime: 2022-05-10 12:22:26
  */
 import React from 'react';
-import { AdapterDayjs as DateAdapter } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import AdapterDayjsLab from '@mui/lab/AdapterDayjs';
+import LocalizationProviderLab from '@mui/lab/LocalizationProvider';
+import { AdapterDayjs as AdapterDayjsX } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider as LocalizationProviderX } from '@mui/x-date-pickers/LocalizationProvider';
 
 const DateLocalizationProvider = (props) => {
-  const { children, dateAdapter, ...restProps } = props;
+  const { children, dateAdapterX, dateAdapterLab, labProps, xProps } = props;
   return (
-    <LocalizationProvider dateAdapter={dateAdapter} {...restProps}>
-      {children}
-    </LocalizationProvider>
+    <LocalizationProviderX dateAdapter={dateAdapterX} {...(xProps || {})}>
+      <LocalizationProviderLab dateAdapter={dateAdapterLab} {...(labProps || {})}>
+        {children}
+      </LocalizationProviderLab>
+    </LocalizationProviderX>
   );
 };
 
 DateLocalizationProvider.defaultProps = {
-  dateAdapter: DateAdapter,
+  dateAdapterX: AdapterDayjsX,
+  dateAdapterLab: AdapterDayjsLab,
 };
 
 export default DateLocalizationProvider;
