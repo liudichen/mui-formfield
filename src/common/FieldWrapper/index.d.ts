44f4ea544235ad24a9e31b3d6b38f90893f0f1b2
iimm <liudichen@foxmail.com>
2022-05-13 10:48:59 +0800

    增加FieldWrapper导出

--- src/common/FieldWrapper/index.d.ts
+++ src/common/FieldWrapper/index.d.ts
@@ -3,12 +3,12 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-15 11:25:34
- * @LastEditTime: 2022-05-10 21:16:55
+ * @LastEditTime: 2022-05-13 10:47:27
  */
 import * as React from 'react';
 import { FieldWrapperRelateProps } from '../../types';
 
-interface FieldWrapperProps extends FieldWrapperRelateProps {
+export interface FieldWrapperProps extends FieldWrapperRelateProps {
   children?: React.ReactNode,
   noFormControl?: boolean,
 }
