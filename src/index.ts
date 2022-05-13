3998e6513bc353ad1905cf5fc46da25c22c6e232
iimm <liudichen@foxmail.com>
2022-05-13 10:48:50 +0800

    增加FieldWrapper导出

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-14 11:01:55
- * @LastEditTime: 2022-05-10 12:24:39
+ * @LastEditTime: 2022-05-13 10:48:17
  */
 export { default as Autocomplete, AutocompleteProps } from './Autocomplete';
 export { default as CheckboxGroup, CheckboxGroupProps } from './CheckboxGroup';
@@ -12,6 +12,7 @@ export { default as DatePicker, DatePickerProps } from './DatePicker';
 export { default as DateRangePicker, DateRangePickerProps } from './DateRangePicker';
 export { default as FieldAction, FieldActionProps } from './FieldAction';
 export { default as FieldLayout, FieldLayoutProps } from './FieldLayout';
+export { default as FieldWrapper, FieldWrapperProps } from './common/FieldWrapper';
 export { default as RadioGroup, RadioGroupProps } from './RadioGroup';
 export { default as Switch, SwitchProps } from './Switch';
 export { default as TextField, TextFieldProps } from './TextField';
