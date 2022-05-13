c57398adaeb064b6840b53b9b205af538d4e91ad
iimm <liudichen@foxmail.com>
2022-05-13 19:58:18 +0800

    ->0.2.22 修复单选组（按钮） 当value值被认为是假值时的问题

--- src/ToggleButtonGroup/index.jsx
+++ src/ToggleButtonGroup/index.jsx
@@ -3,7 +3,7 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-12 15:11:12
- * @LastEditTime: 2022-05-13 14:12:04
+ * @LastEditTime: 2022-05-13 19:55:38
  */
 import PropTypes from 'prop-types';
 import { useMemoizedFn, useSafeState } from 'ahooks';
@@ -25,7 +25,7 @@ const ToggleButtonGroup = (props) => {
   const [ options, setOptions ] = useSafeState([]);
   const [ optionsValues, setOptionsValues ] = useSafeState([]);
   const [ loading, setLoading ] = useSafeState(false);
-  const [ value, onChange ] = useMergedState(defaultValue, { value: valueProp, onChange: onChangeProp, postState: (s) => s || (exclusive ? null : []) });
+  const [ value, onChange ] = useMergedState(defaultValue, { value: valueProp, onChange: onChangeProp, postState: (s) => s || (exclusive ? s : []) });
 
   const fetchOptions = useMemoizedFn(async () => {
     setLoading(true);
