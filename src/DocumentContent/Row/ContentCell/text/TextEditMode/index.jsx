b01f9ecd18585e29dd91e15e1573577b78733c89
iimm <liudichen@foxmail.com>
2022-05-20 22:31:31 +0800

    文本编辑添加清除按钮

--- src/DocumentContent/Row/ContentCell/text/TextEditMode/index.jsx
+++ src/DocumentContent/Row/ContentCell/text/TextEditMode/index.jsx
@@ -3,17 +3,18 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-05-16 16:35:09
- * @LastEditTime: 2022-05-18 20:01:36
+ * @LastEditTime: 2022-05-20 22:30:29
  */
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useMemoizedFn } from 'ahooks';
-import { TextField } from '@mui/material';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
+import { IconX } from '@tabler/icons';
 
 const TextEditMode = (props) => {
   const { text, handleChange, id } = props;
   const onChange = useMemoizedFn((e) => {
-    handleChange(id, { text: { ...text, text: e.target.value || '', type: '文本' } });
+    handleChange(id, { text: { ...text, text: e?.target?.value || '', type: '文本' } });
   });
   return (
     <TextField
@@ -22,6 +23,19 @@ const TextEditMode = (props) => {
       multiline
       value={text?.text}
       onChange={onChange}
+      InputProps={{
+        endAdornment: text ? (
+          <InputAdornment position='end'>
+            <IconButton
+              tabIndex={-1}
+              sx={{ ml: -2 }}
+              onClick={() => onChange()}
+            >
+              <IconX size='20px'/>
+            </IconButton>
+          </InputAdornment>
+        ) : null,
+      }}
     />
   );
 };
