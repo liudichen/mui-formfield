ec7789275d4639361c5153f22b5f1b3c15e33524
liudichen <liudichen@foxmail.com>
2022-10-23 17:34:25 +0800

    ->0.3.5移除richText组件

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: 柳涤尘 https://www.iimm.ink
  * @LastEditors: 柳涤尘 liudichen@foxmail.com
  * @Date: 2022-04-14 11:01:55
- * @LastEditTime: 2022-10-23 00:01:05
+ * @LastEditTime: 2022-10-23 17:33:21
  */
 
 // contextProvider
@@ -32,7 +32,7 @@ export { default as EditableTable, EditableTableProps, EditModalProps,
 export { default as DocumentContent, DocumentContentProps, IRowItem as DocumentCOntentRowItem } from './Fields/DocumentContent';
 export { documentContentDataPurify } from './Fields/DocumentContent/utils';
 export { default as KeyWords, KeyWordsProps } from './Fields/KeyWords';
-export { default as RichText, RichTextProps } from './Fields/RichText';
+// export { default as RichText, RichTextProps } from './Fields/RichText';
 
 // Formily组件
 
